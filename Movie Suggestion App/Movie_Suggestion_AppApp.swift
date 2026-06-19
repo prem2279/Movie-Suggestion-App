0652183e95631d68f0337d64619c099f6d053dd3
Prem Gundu <60621227+prem2279@users.noreply.github.com>
2026-06-19 15:28:04 -0400

    Initial Commit

---
+++ Movie Suggestion App/Movie_Suggestion_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Suggestion_AppApp.swift
+//  Movie Suggestion App
+//
+//  Created by Prem Kumar Gundu on 6/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Suggestion_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movie Suggestion AppTests/Movie_Suggestion_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  Movie_Suggestion_AppTests.swift
+//  Movie Suggestion AppTests
+//
+//  Created by Prem Kumar Gundu on 6/19/26.
+//
+
+import Testing
+@testable import Movie_Suggestion_App
+
+struct Movie_Suggestion_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
